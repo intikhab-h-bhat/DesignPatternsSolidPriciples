1e206cf98748af1c925552c3dab56323f09c829a
intikhab-h-bhat <intikhabh@gmail.com>
2024-09-26 21:41:10 +0530

    BadBankAccount without Enacapsulation

--- DesignPatternsSolidPriciples/Program.cs
+++ DesignPatternsSolidPriciples/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DesignPatternsSolidPriciples.Encapsulation;
+
 Console.WriteLine("Hello, World!");
+
+BadBankAcount ba=new BadBankAcount();
+
+ba.balance = 500;
+
+Console.WriteLine(ba.balance);
