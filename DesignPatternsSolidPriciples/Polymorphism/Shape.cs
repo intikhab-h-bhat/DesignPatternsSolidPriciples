0b734f9754a11988354f303a161d005c94e476aa
intikhab-h-bhat <intikhabh@gmail.com>
2024-10-02 08:55:26 +0530

    Polymorphism

---
+++ DesignPatternsSolidPriciples/Polymorphism/Shape.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsSolidPriciples.Polymorphism
+{
+    public class Shape
+    {
+        public int Sides {  get; set; }
+        public string Name { get; set; }
+
+        public virtual void ClosedFig()
+        {
+            Console.WriteLine("Closed Figure");
+        }
+
+    }
+
+    public class Square:Shape
+    {
+
+        public override void ClosedFig()
+        {
+            //base.ClosedFig();
+            Console.WriteLine("Square Closed Figure");
+        }
+
+    }
+    public class Line : Shape
+    {
+
+        public override void ClosedFig()
+        {
+            //base.ClosedFig();
+            Console.WriteLine("Not closed Figure");
+        }
+    }
+}
--- DesignPatternsSolidPriciples/Program.cs
+++ DesignPatternsSolidPriciples/Program.cs
@@ -2,25 +2,49 @@
 using DesignPatternsSolidPriciples.Abstraction;
 using DesignPatternsSolidPriciples.Encapsulation;
 using DesignPatternsSolidPriciples.Inheritance;
+using DesignPatternsSolidPriciples.Polymorphism;
+using System;
 
 
+Console.WriteLine("**************Polymorphism*****************");
 
-Console.WriteLine("*****************Inheritance Example************");
+//List<Object> shapes = new List<Object>();
+List<Shape> shapes = new List<Shape>();
+shapes.Add(new Square { Name = "Square",Sides=4 });
+shapes.Add(new Line { Name = "Line", Sides = 0 });
 
-var car=new Car();
-Console.WriteLine(car.Brand = "Toyoto");
-Console.WriteLine(car.NumberOfDoors = 4);
-car.Model = "abc";
-car.Start();
-car.Stop();
+foreach(var s in shapes)
+{
+    //if (s is Square)
+    //{
+    //    var sq = new Square();
+    //    sq.ClosedFig();
 
-var bike=new Bike();
-Console.WriteLine(bike.Brand = "Hero Honda");
-Console.WriteLine($"Number of wheels:  {bike.NumberOfWheels = 2}");
-bike.Start();
+    //}
+    s.ClosedFig();
 
-bike.Stop();
     
+}
+
+
+
+
+//Console.WriteLine("*****************Inheritance Example************");
+
+//var car=new Car();
+//Console.WriteLine(car.Brand = "Toyoto");
+//Console.WriteLine(car.NumberOfDoors = 4);
+//car.Model = "abc";
+//car.Start();
+//car.Stop();
+
+//var bike=new Bike();
+//Console.WriteLine(bike.Brand = "Hero Honda");
+//Console.WriteLine($"Number of wheels:  {bike.NumberOfWheels = 2}");
+//bike.Start();
+
+//bike.Stop();
+
 
 
 
